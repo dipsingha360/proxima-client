4d0bc15ec131232ad2b60b0e9603098baee8aa8f
Dip Singha <dipsingha2@gmail.com>
2023-02-17 16:53:09 +0600

    proxima client project init and nav created

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="navbar container mx-auto h-20 flex items-center border-b border-sky-900">
+      <Link to={"/"} className="logo text-3xl font-semibold text-sky-400">
+        Proxima.
+      </Link>
+    </div>
+  );
+};
+
+export default Navbar;
