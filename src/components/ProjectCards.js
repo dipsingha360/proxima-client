0da5b030e286a50cd8df6aeffaf9867eab28477a
Dip Singha <dipsingha2@gmail.com>
2023-02-17 18:11:35 +0600

    add home section with projects cards, data fetching from backend

---
+++ src/components/ProjectCards.js
@@ -0,0 +1,44 @@
+import { currencyFormatter } from "../utils/currencyFormatter";
+
+const ProjectCards = ({ project }) => {
+  return (
+    <div className="project flex flex-col gap-5 bg-slate-800 p-5 rounded-xl shadow-md border border-slate-500 w-[30rem]">
+      <div className="top">
+        <span className="text-sky-400">ID: {project._id}</span>
+        <h3 className="text-3xl font-medium">{project.title}</h3>
+        <span className="uppercase text-xs tracking-widest text-slate-500">
+          {project.tech}
+        </span>
+      </div>
+      <div className="mid text-slate-300 flex gap-5">
+        <div className="mid-left flex flex-col">
+          <span>Budget: {currencyFormatter(project.budget)}</span>
+          <span>
+            Add on: {new Date(project.createdAt).toLocaleDateString()}
+          </span>
+          <span>
+            Last updated: {new Date(project.updatedAt).toLocaleDateString()}
+          </span>
+        </div>
+        <div className="mid-right flex flex-col">
+          <span>Manager: {currencyFormatter(project.manager)}</span>
+          <span>Developers: {project.dev}</span>
+          <span>
+            Duration:{" "}
+            {`${project.duration} week${project.duration === 1 ? "" : "s"}`}
+          </span>
+        </div>
+      </div>
+      <div className="bottom flex gap-5">
+        <button className="btn-update bg-sky-400 text-slate-900 py-2 px-5 rounded shadow-xl hover:bg-sky-50 duration-300 ">
+          Update
+        </button>
+        <button className="btn-delete text-rose-500 hover:underline">
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCards;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,44 @@
+import { useEffect, useState } from "react";
+import ProjectCards from "../components/ProjectCards";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getProject = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/api/projects");
+        if (!res.ok) throw new Error("Someting went wrong");
+        const data = await res.json();
+        setProjects(data);
+        setLoading(false);
+        console.log(data);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+    getProject();
+  }, []);
+
+  return (
+    <div className="home container mx-auto py-20 grid grid-cols-3 gap-10">
+      <div className="left col-span-2 ">
+        <h2 className="text-4xl font-medium text-sky-400 mb-10 ">
+          All projects
+        </h2>
+        <div className="projects-wrapper flex gap-10 flex-wrap">
+          {projects &&
+            projects.map((project) => (
+              <ProjectCards key={project._id} project={project} />
+            ))}
+        </div>
+      </div>
+      <div className="right"></div>
+    </div>
+  );
 };
 
 export default Home;
