af134f80032963bdbac23356ed42609db5c7214b
Dip Singha <dipsingha2@gmail.com>
2023-03-03 13:55:14 +0600

    login and signup navigation added on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,18 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="navbar container mx-auto h-20 flex items-center border-b border-sky-900">
+    <div className="navbar container mx-auto h-20 flex justify-between items-center border-b border-sky-900">
       <Link to={"/"} className="logo text-3xl font-semibold text-sky-400">
         Proxima.
       </Link>
+      <nav className=" flex gap-5">
+        <Link className="hover:text-sky-400 duration-300" to="/login">
+          Login
+        </Link>
+        <Link className="hover:text-sky-400 duration-300" to="/signup">
+          Signup
+        </Link>
+      </nav>
     </div>
   );
 };
