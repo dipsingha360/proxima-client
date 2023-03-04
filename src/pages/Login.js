c4419b59b032dc3ce4a5033067db9b8c73eb4ac5
Dip Singha <dipsingha2@gmail.com>
2023-03-04 11:45:19 +0600

    login functionality added on frontend

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,28 @@
 import { useState } from "react";
+import { useLogin } from "../hooks/useLogin";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const { login, error, loading } = useLogin();
+
+  const handleeLogin = async (e) => {
+    e.preventDefault();
+
+    // login user
+    await login(email, password);
+    if (!error) {
+      setEmail("");
+      setPassword("");
+    }
+  };
+
   return (
-    <form className="login-form max-w-sm mx-auto flex flex-col gap-5 py-20 ">
+    <form
+      onSubmit={handleeLogin}
+      className="login-form max-w-sm mx-auto flex flex-col gap-5 py-20 "
+    >
       <h2 className="text-4xl font-medium text-sky-400 mb-10 ">Login</h2>
 
       <div className="form-control flex flex-col gap-2">
@@ -43,11 +60,17 @@ const Login = () => {
       </div>
 
       <button
+        disabled={loading}
         type="submit"
         className="btn bg-sky-400 text-slate-900 py-3 mt-3  rounded-xl hover:bg-sky-500 duration-300"
       >
         Login
       </button>
+      {error && (
+        <p className="bg-yellow-500/20 rounded-lg p-5 text-yellow-500 border border-yellow-500">
+          {error}
+        </p>
+      )}
     </form>
   );
 };
