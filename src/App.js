ab1ab64ab18fba5c8578afba4e7e8ee4ccae2bb2
Dip Singha <dipsingha2@gmail.com>
2023-03-05 15:19:04 +0600

    fix some footer issue

--- src/App.js
+++ src/App.js
@@ -10,22 +10,27 @@ const App = () => {
   const { user } = useAuthContext();
 
   return (
-    <div className="app bg-slate-900 text-slate-100 min-h-screen">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
-        <Route
-          path="/login"
-          element={!user ? <Login /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/signup"
-          element={!user ? <Signup /> : <Navigate to="/" />}
-        />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+    <>
+      <div className="app bg-slate-900 text-slate-100 min-h-screen">
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={user ? <Home /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/login"
+            element={!user ? <Login /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/signup"
+            element={!user ? <Signup /> : <Navigate to="/" />}
+          />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
       <Footer />
-    </div>
+    </>
   );
 };
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,9 @@
 const Footer = () => {
   return (
-    <div className="footer  py-5 flex items-center justify-center bg-slate-800/20">
-      <p>&copy; {new Date().getFullYear()} Proxima. All right reserved.</p>
+    <div className="footer  py-5 flex items-center justify-center bg-slate-900/95">
+      <p className="text-slate-400">
+        &copy; {new Date().getFullYear()} Proxima. All right reserved.
+      </p>
     </div>
   );
 };
