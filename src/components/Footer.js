3c58aab8eb292d64c708b230cced9818349d09ea
Dip Singha <dipsingha2@gmail.com>
2023-03-05 15:09:06 +0600

    footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer  py-5 flex items-center justify-center bg-slate-800/20">
+      <p>&copy; {new Date().getFullYear()} Proxima. All right reserved.</p>
+    </div>
+  );
 };
 
 export default Footer;
